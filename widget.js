71e7c77be151b9eb0fff08ffe6b753b8fbc59b02
nnms.sh <khamidullinavaleriia@gmail.com>
2026-05-03 13:18:24 +0300

    Update widget.js

--- widget.js
+++ widget.js
@@ -3,7 +3,7 @@
 // Установка: вставь этот скрипт в Scriptable, добавь виджет на экран блокировки
 //
 // ⚙️ НАСТРОЙКА: вставь свой Gist URL ниже
-const GIST_URL = "https://gist.githubusercontent.com/YOUR_USER/51d6b57bfb95eb43af1d0d3dbf971547/raw/cm_tasks.json";
+const GIST_URL = "https://gist.githubusercontent.com/YOUR_USER/f9717592e4d432304943d612178e042a/raw/cm_tasks.json";
 // Как получить URL: см. инструкцию в мини-апп → кнопка "Синхронизировать с виджетом"
 
 const TAG_EMOJI = {
